ab5a523c4e1a73a0e2f1f98211a38b471a6602c8
Arkopravo Bhuin <99447101+Arkopravo1995@users.noreply.github.com>
2024-09-27 08:17:21 +0000

    abcd

---
+++ numberupdate_pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int n = 4;
+
+int main(){
+// int num = 1;
+char ch='A';
+for(int i = 0 ; i < n ;i++){
+    
+    for(int j = 0 ; j < n ;  j++){
+        cout<< ch;
+        ch++;
+    }
+    cout<< endl;
+
+}
+}
